81a8ce576b75fdf0cab88086a9ecdc301ab7dcab
Augusto <augustomp55@gmail.com>
2017-12-24 17:37:23 -0500

    V20

--- CSharp/Halite2/MyBot.cs
+++ CSharp/Halite2/MyBot.cs
@@ -3,53 +3,98 @@ using System.Collections.Generic;
 
 namespace Halite2
 {
-    public class MyBot
+  public class MyBot
+  {
+
+    public static void Main(string[] args)
     {
+      string name = args.Length > 0 ? args[0] : "Sharpie";
 
-        public static void Main(string[] args)
+      Networking networking = new Networking();
+      GameMap gameMap = networking.Initialize(name);
+      Planet largestPlanet;
+
+      List<Planet> selectedPlanets = new List<Planet>();
+
+      List<Move> moveList = new List<Move>();
+      for (; ; )
+      {
+        moveList.Clear();
+        gameMap.UpdateMap(Networking.ReadLineIntoMetadata());
+
+        foreach (Ship ship in gameMap.GetMyPlayer().GetShips().Values)
         {
-            string name = args.Length > 0 ? args[0] : "Sharpie";
 
-            Networking networking = new Networking();
-            GameMap gameMap = networking.Initialize(name);
+          if (ship.GetDockingStatus() != Ship.DockingStatus.Undocked)
+          {
+            continue;
+          }
 
-            List<Move> moveList = new List<Move>();
-            for (; ; )
+          foreach (Planet planet in gameMap.GetAllPlanets().Values)
+          {
+            if (planet.IsOwned())
+              break;
+
+            largestPlanet = gameMap.GetPlanet(4);
+
+            if (planet.GetRadius() > largestPlanet.GetRadius())
             {
-                moveList.Clear();
-                gameMap.UpdateMap(Networking.ReadLineIntoMetadata());
-
-                foreach (Ship ship in gameMap.GetMyPlayer().GetShips().Values)
-                {
-                    if (ship.GetDockingStatus() != Ship.DockingStatus.Undocked)
-                    {
-                        continue;
-                    }
-
-                    foreach (Planet planet in gameMap.GetAllPlanets().Values)
-                    {
-                        if (planet.IsOwned())
-                        {
-                            continue;
-                        }
-
-                        if (ship.CanDock(planet))
-                        {
-                            moveList.Add(new DockMove(ship, planet));
-                            break;
-                        }
-
-                        ThrustMove newThrustMove = Navigation.NavigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED / 2);
-                        if (newThrustMove != null)
-                        {
-                            moveList.Add(newThrustMove);
-                        }
-
-                        break;
-                    }
-                }
-                Networking.SendMoves(moveList);
+              largestPlanet = planet;
+              break;
             }
+
+            foreach (Planet sPlanet in selectedPlanets)
+            {
+              if (sPlanet == largestPlanet)
+                break;
+            }
+         
+            if (ship.CanDock(largestPlanet))
+            {
+              moveList.Add(new DockMove(ship, largestPlanet));
+              selectedPlanets.Add(largestPlanet);
+              break;
+            }
+            else
+            {
+              ThrustMove newLThrustMove = Navigation.NavigateShipToDock(gameMap, ship, largestPlanet, Constants.MAX_SPEED);
+              if (newLThrustMove != null)
+              {
+                moveList.Add(newLThrustMove);
+                selectedPlanets.Add(largestPlanet);
+                break;
+              }
+            }
+
+            foreach (Planet sPlanet in selectedPlanets)
+            {
+              if (sPlanet == planet)              
+                break;
+            }
+
+            if (ship.CanDock(planet))
+            {                        
+              moveList.Add(new DockMove(ship, planet));
+              selectedPlanets.Add(planet);
+              break;
+            }
+            else
+            {
+
+              ThrustMove newThrustMove = Navigation.NavigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED);
+              if (newThrustMove != null)
+              {
+                moveList.Add(newThrustMove);
+                selectedPlanets.Add(largestPlanet);
+              }
+
+              break;
+            }
+          }
         }
+        Networking.SendMoves(moveList);
+      }
     }
+  }
 }
+
